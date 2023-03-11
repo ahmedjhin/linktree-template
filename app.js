c77cf4b99d03b9b4aeba919be0a25ff355c7668b
ahmedjhin <a7madti90@hotmail.com>
2023-03-12 03:46:23 +0400

    finished

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 const shareButtons = document.querySelectorAll('.tile-share-button')
 console.log(shareButtons)
 
-function copyText(e) {
+async function copyText(e) {
     e.preventDefault()
+    const link = this.getAttribute('link')
+    console.log(link)
+    try {
+        await navigator.clipboard.writeText(link)
+        alert("Copied the text: " + link)
+    } catch (err) {
+        console.error(err)
+    }
     
 }
 
